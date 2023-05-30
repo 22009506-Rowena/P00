3f3853c5858a00c819ccc40c249f991d79896f31
22009506 <22009506@10.172.170.217>
2023-05-30 12:03:59 +0800

    Calculator.java-changecomment CalculatorTest.java-changecomment

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,81 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+
+		public void testAdd() {
+			int a = 4321;
+			int b = 1234;
+				
+			Calculator cal = new Calculator();
+			int actual = cal.add(a, b); 
+				 
+			int expected = 5555;
+			assertEquals (expected, actual);
+			}
+
+	@Test
+	public void testsubstract() {
+		int a = 4321;
+		int b = 1234;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.substract(a, b); 
+			 
+		int expected = 3087;
+		assertEquals (expected, actual);
+		}
+	@Test
+	public void testmultiple() {
+		int a = 4;
+		int b = 2;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.multiply(a, b); 
+			 
+		int expected = 8;
+		assertEquals (expected, actual);
+		}
+	@Test
+	public void testdivide() {
+		int a = 4;
+		int b = 2;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.divide(a, b); 
+			 
+		int expected = 2;
+		assertEquals (expected, actual);
+		}
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	
+
+	
+	
+	
+	
+	
+
+
+}
